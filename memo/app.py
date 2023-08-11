490e69e85e391cfd63c3c634b6976cf672b14d84
Yabsera Bogale <yabsera@debian.YabseraBogale.com>
2023-08-11 19:41:16 +0300

    Added memo biolerplate for it

--- memo/app.py
+++ memo/app.py
@@ -6,6 +6,10 @@ app=Flask(__name__)
 @app.route('/')
 def index():
     return render_template('home.html')
+@app.route('/memo.html')
+def memo():
+    return render_template('memo.html')
+
 
 if __name__=="__main__":
     app.run(debug=True)
